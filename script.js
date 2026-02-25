73bbc044501f6e1497629b79f9f141db6edf3895
SelmaMiski <selmamiski@icloud.com>
2026-02-26 00:45:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ const navLinks = document.getElementById("nav-links");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+// auto close mobile menu on click
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});// navbar scroll effect
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 40) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
